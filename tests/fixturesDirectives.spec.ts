27ab3b40507932f6502148ee74315424beaabbb3
Chris Sewell <chrisj_sewell@hotmail.com>
2022-01-21 12:10:01 +0100

    🧪 TESTS: Add testing of math directive

--- tests/fixturesDirectives.spec.ts
+++ tests/fixturesDirectives.spec.ts
@@ -11,3 +11,16 @@ describe("Parses directives", () => {
     it(name, () => expect(rendered.trim()).toEqual((expected || "").trim()))
   })
 })
+
+describe("Parses math directives", () => {
+  readFixtures("directives.math").forEach(([name, text, expected]) => {
+    const mdit = MarkdownIt().use(docutils_plugin)
+    // TODO for now we add a basic renderer
+    mdit.renderer.rules.math_block = (tokens, idx) => {
+      return `<div class="math block">\n${tokens[idx].content.trimRight()}\n</div>`
+    }
+    const rendered = mdit.render(text)
+    // console.log(rendered)
+    it(name, () => expect(rendered.trim()).toEqual((expected || "").trim()))
+  })
+})
