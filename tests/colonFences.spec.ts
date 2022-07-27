ea5b1658c42dc95eeb6ee7bf9322c4a524203772
Rowan Cockett <rowanc1@gmail.com>
2022-07-27 11:38:57 -0600

    🐛 Use colonFence from myst-extras

--- tests/colonFences.spec.ts
+++ tests/colonFences.spec.ts
@@ -1,16 +1,19 @@
 /* eslint-disable jest/valid-title */
 import MarkdownIt from "markdown-it"
+import { colonFencePlugin } from "markdown-it-myst-extras"
 import docutils_plugin from "../src"
 import readFixtures from "./readFixtures"
 
+// We have to test compatibility with colonFences here!
+
 describe("Parses colonFences", () => {
   test("colon fences parse", () => {
-    const mdit = MarkdownIt().use(docutils_plugin)
+    const mdit = MarkdownIt().use(colonFencePlugin).use(docutils_plugin)
     const parse = mdit.parse(`:::{tip}\nThis is a tip in a fence!\n:::`, {})
     expect(parse[0].type).toBe("parsed_directive_open")
   })
   test("colon fences render", () => {
-    const mdit = MarkdownIt().use(docutils_plugin)
+    const mdit = MarkdownIt().use(colonFencePlugin).use(docutils_plugin)
     const rendered = mdit.render(`:::{tip}\nfence\n:::`)
     expect(rendered.trim()).toEqual(
       '<aside class="admonition tip">\n<header class="admonition-title">Tip</header>\n<p>fence</p>\n</aside>'
@@ -20,7 +23,7 @@ describe("Parses colonFences", () => {
 
 describe("Parses fenced directives", () => {
   readFixtures("directives.fence").forEach(([name, text, expected]) => {
-    const mdit = MarkdownIt().use(docutils_plugin)
+    const mdit = MarkdownIt().use(colonFencePlugin).use(docutils_plugin)
     const rendered = mdit.render(text)
     it(name, () => expect(rendered.trim()).toEqual((expected || "").trim()))
   })
